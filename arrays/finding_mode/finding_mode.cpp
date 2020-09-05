cb82cdba776809068a176f061989ef5a288da465
Ashley Laswell <lswl9898@gmail.com>
2020-09-05 12:26:01 -0400

    finding mode if numbers are grouped together

--- arrays/finding_mode/finding_mode.cpp
+++ arrays/finding_mode/finding_mode.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 int main(){
 	
-		
-	
-	return 0;
+	const int ARRAY_SIZE = 12;
+	int surveyData[ARRAY_SIZE] = {4, 7, 7, 9, 9, 9, 8, 3, 3, 3, 3, 10};
 
+	int mostFrequent;
+	int highestFrequency = 0;
+	int currentFrequency = 0;
+	
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		currentFrequency++;
+		//if surveyData[i] is last last occurence of a value
+		if (i == ARRAY_SIZE - 1 || surveyData[i] != surveyData[i + 1]) {
+			if (currentFrequency > highestFrequency) {
+				highestFrequency = currentFrequency;
+				mostFrequent = surveyData[i];
+			}
+			currentFrequency = 0;
+		}
+	}
+	cout << mostFrequent << "\n";
+
+	return 0;
 }
