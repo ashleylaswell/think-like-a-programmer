8c40e5df80914f145061ba429e1b84f9cd0e7893
Ashley Laswell <lswl9898@gmail.com>
2020-09-08 13:21:11 -0400

    tracked highest grade with array position

--- arrays/highest_grade/highest_grade.cpp
+++ arrays/highest_grade/highest_grade.cpp
@@ -10,7 +10,7 @@ int main() {
 	};
 
 	const int ARRAY_SIZE = 10;
-	student studentArrat[ARRAY_SIZE] = {
+	student studentArray[ARRAY_SIZE] = {
 		{87, 10001, "Fred"},
 		{28, 10002, "Tom"},
 		{100, 10003, "Alistar"},
@@ -21,5 +21,12 @@ int main() {
 		{70, 10008, "Candy"},
 		{81, 10009, "Aretha"},
 		{68, 10010, "Veronica"},
+	};
+	int highPosition = 0;
+	for (int i = 1; i < ARRAY_SIZE; i++) {
+		if (studentArray[i].grade > studentArray[highPosition].grade) {
+			highPosition = i;
+		}
 	}
+	cout << highPosition << "\n";
 }
