cbe1afd87c043c32236d8a6927933f70f7a9508b
Ashley Laswell <lswl9898@gmail.com>
2020-09-08 15:50:35 -0400

    added dynamic array file

---
+++ arrays/dynamic_array/dynamic_array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int ARRAY_SIZE;
+	cout << "number of survey responses: ";
+	cin >> ARRAY_SIZE;
+	int *surveyData = new int [ARRAY_SIZE];
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		cout << "survey response " << i + 1 << ": ";
+		cin >> surveyData[i];
+	}
+}
