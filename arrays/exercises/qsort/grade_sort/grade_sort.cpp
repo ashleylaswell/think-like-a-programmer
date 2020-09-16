b29d65be89c735a54a36694fd9a362024c6b17b0
Ashley Laswell <lswl9898@gmail.com>
2020-09-16 16:29:05 -0400

    starting over

--- arrays/exercises/qsort/grade_sort/grade_sort.cpp
+++ arrays/exercises/qsort/grade_sort/grade_sort.cpp
@@ -2,48 +2,35 @@
 using namespace std;
 
 //comparator function - sorts from smallest to largest
-//int compareFunc(const void * voidA, const void * voidB){
-//	int * intA = (int *)(voidA);
-//	int * intB = (int *)(voidB);
+int compareFunc(const void * voidA, const void * voidB){
+	int * intA = (int *)(voidA);
+	int * intB = (int *)(voidB);
 		
-//	return *intA - *intB;
-//}
-
-int compareFunc(const void *a, const void *b) {
-	struct student *intA = (struct student *)a;
-	struct student *intB = (struct student *)b;
-	return (int)(intA->grade - intB->grade);
+	return *intA - *intB;
 }
 
-	struct student {
-		int grade;
-		int studentID;
-		string name;
-	};
+struct student {
+	int grade;
+	int studentID;
+	string name;
+};
 
-	const int ARRAY_SIZE = 10;
-	student studentArray[ARRAY_SIZE] = {
-		{87, 10001, "Fred"},
-		{28, 10002, "Tom"},
-		{100, 10003, "Alistar"},
-		{78, 10004, "Sasha"},
-		{84, 10005, "Erin"},
-		{98, 10006, "Belinda"},
-		{75, 10007, "Leslie"},
-		{70, 10008, "Candy"},
-		{81, 10009, "Aretha"},
-		{68, 10010, "Veronica"},
-	};
+const int ARRAY_SIZE = 10;
+student studentArray[ARRAY_SIZE] = {
+	{87, 10001, "Fred"},
+	{28, 10002, "Tom"},
+	{100, 10003, "Alistar"},
+	{78, 10004, "Sasha"},
+	{84, 10005, "Erin"},
+	{98, 10006, "Belinda"},
+	{75, 10007, "Leslie"},
+	{70, 10008, "Candy"},
+	{81, 10009, "Aretha"},
+	{68, 10010, "Veronica"},
+};
+
 int main(){
-
 	qsort(studentArray, ARRAY_SIZE, sizeof(int), compareFunc);
-
-	int sortedArray[ARRAY_SIZE];
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		sortedArray[i] = studentArray[i];
-		cout << sortedArray[i] << " ";
-	}
-	cout << "\n";
 
 }
 
