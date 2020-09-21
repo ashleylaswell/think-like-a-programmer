0f51910cb614e828ad0e1cd67a9477c1dc7d39c5
Ashley Laswell <lswl9898@gmail.com>
2020-09-21 17:11:00 -0400

    starting sub cipher

---
+++ arrays/exercises/sub_cipher/subCipher.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+const char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int main() {
+	
+	
+}
